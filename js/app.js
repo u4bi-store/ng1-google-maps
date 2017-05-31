b6c5d768fb5fb8f4f6af4b769fbb1a9e3deeb417
Yu Myungjae <u4bi@letsee.io>
2017-05-31 20:41:26 +0900

    basic map render

--- js/app.js
+++ js/app.js
@@ -1,14 +1,16 @@
 var app = angular.module('app', ['ngMap']);
 
-app.controller('ctrl', function(NgMap) {
-  
-  NgMap.getMap().then(function(map) {
-    console.log(map);
-    
-    console.log('위도 : ', map.getCenter().lat(), '경도 : ', map.getCenter().lng());
+app.controller('ctrl', function($scope, NgMap) {
 
-    console.log('마커 : ', map.markers);
-    console.log('원  : ', map.shapes);
-  });
+    $scope.API_KEY = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyBQ5Y0xV98nz7z-8X3xjLlTqPdfekGuNp0';
+
+    NgMap.getMap().then(function(map) {
+        console.log(map);
+
+        console.log('위도 : ', map.getCenter().lat(), '경도 : ', map.getCenter().lng());
+
+        console.log('마커 : ', map.markers);
+        console.log('원  : ', map.shapes);
+    });
 
 });
