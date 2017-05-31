f7fa6d5ba576f465cd2bbccfa20f60507ef3ffb2
Yu Myungjae <u4bi@letsee.io>
2017-05-31 20:30:48 +0900

    - lazy load 형태의 구글맵 출력

--- js/app.js
+++ js/app.js
@@ -1,6 +1,14 @@
 var app = angular.module('app', ['ngMap']);
 
 app.controller('ctrl', function(NgMap) {
-    console.log(NgMap);
+  
+  NgMap.getMap().then(function(map) {
+    console.log(map);
+    
+    console.log('위도 : ', map.getCenter().lat(), '경도 : ', map.getCenter().lng());
+
+    console.log('마커 : ', map.markers);
+    console.log('원  : ', map.shapes);
+  });
 
 });
